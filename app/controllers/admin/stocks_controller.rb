03cfccc66ab7f9ac04b43c772bf9565fcb80a54b
Olivier <olivier.simart@gmail.com>
2013-04-12 01:07:07 -0400

    Stocks cleanup. closed #4

--- app/controllers/admin/stocks_controller.rb
+++ app/controllers/admin/stocks_controller.rb
@@ -14,14 +14,9 @@ class Admin::StocksController < Admin::BaseController
     begin
       params[:stocks].values.first.values.each do |product_params|
         Product.find(product_params.delete(:id)).update_attributes!(product_params)
-        # only ratio with id or star present is applicable to proceed
-        # unless ratio_params.select{|k, v| k =~ /^(star|id)/ && !v.empty?}.empty?
-        #   ratio = Ratio.find_by_id(ratio_params.delete(:id)) || Ratio.new
-        #   ratio.update_attributes!(ratio_params)
-        # end
       end
     rescue
-      return redirect_to [:admin, :stocks], alert: "Failed to update stocks. Please check inputs."
+      return redirect_to [:admin, :stocks], alert: "Failed to update stocks. Please check input."
     end
     
     return redirect_to [:admin, :stocks], notice: "Stocks successfully updated."
