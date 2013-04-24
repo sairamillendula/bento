4b60637bc394a3dd5e6c4b15a45da31ae9f97c2e
Olivier <olivier.simart@gmail.com>
2013-04-24 09:37:39 -0400

    ProductReviews controller cleanup

--- app/controllers/product_reviews_controller.rb
+++ app/controllers/product_reviews_controller.rb
@@ -1,13 +1,8 @@
 class ProductReviewsController < ApplicationController
   
-
-
 	def create
-    #@product = Product.find(params[:slug])
-    #@product = Product.find(:product_id)
     @review = ProductReview.new(params[:product_review])
     @review.user_id = current_user.id if current_user
-    #@review.product_id = Product.find(params[:id]).id
 
     respond_to do |format|
       if @review.save
