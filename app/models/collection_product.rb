2653875093f3d08abc4cb26178c1f2ff8ce3f3d8
Olivier <olivier.simart@gmail.com>
2013-04-24 11:04:48 -0400

    Updated collection. closed #15. Admin style (btn)

--- app/models/collection_product.rb
+++ app/models/collection_product.rb
@@ -2,7 +2,7 @@ class CollectionProduct < ActiveRecord::Base
 	belongs_to :collection
 	belongs_to :product
 
-  acts_as_list :scope => :collection
+  acts_as_list scope: :collection
 	
   attr_accessible :collection_id, :position, :product_id
 end
