c842774a7e593037948e05a3b490d618d3cc886c
Olivier <olivier.simart@gmail.com>
2013-04-25 00:57:53 -0400

    Major style update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,7 +39,7 @@ module ApplicationHelper
 
   def page_title(title, &block)
     page_actions = block_given? ? capture(&block) : ''
-    content_tag(:div, class: "page-header") do
+    content_tag(:div, class: "page-title") do
       content_tag(:h1, (title + page_actions).html_safe)
     end
   end
