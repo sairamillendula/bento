c840d39cbc1138af31856595ed49baac3b5708f8
Olivier <olivier@imac-de-olivier.gateway.2wire.net>
2013-06-20 10:18:52 -0400

    Compiling theme assets

--- config/application.rb
+++ config/application.rb
@@ -60,5 +60,6 @@ module Bento
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    config.assets.precompile += ['theme.js', 'theme.css']
   end
 end
