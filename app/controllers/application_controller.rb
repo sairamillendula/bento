716a238969a3e7c53507587abb1b377798fb20ec
Olivier <olivier.simart@gmail.com>
2013-06-28 00:18:18 -0400

    Fix for locale if visitor is a robot

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 private
 
   def set_locale
-    I18n.locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first || I18n.default_locale
+    I18n.locale = request.env['HTTP_ACCEPT_LANGUAGE'].try(:scan, /^[a-z]{2}/).try(:first) || I18n.default_locale
   end
 
   def load_cart
