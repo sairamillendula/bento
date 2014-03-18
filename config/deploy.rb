4a8815e1d3b99d59b54c015e2d2bc9291e3d1ae5
Olivier <olivier.simart@gmail.com>
2014-03-18 19:12:50 -0400

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -125,7 +125,7 @@ namespace :git do
   task :push_deploy_tag do
     user = `git config --get user.name`.chomp
     email = `git config --get user.email`.chomp
-    puts `git tag #{stage}-deploy-#{release_name} #{current_revision} -m "Deployed by #{user} <#{email}>"`
+    puts `git tag #production-deploy-#{release_name} #{current_revision} -m "Deployed by #{user} <#{email}>"`
     puts `git push --tags origin`
   end
 end
