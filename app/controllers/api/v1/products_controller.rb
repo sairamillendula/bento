d72ce95aa94b41f73b21071303772c5732085c90
Gennady Kudelya <gennady.kudelya@gmail.com>
2014-03-21 11:56:39 +0200

    [#77] api json [t: 2:40[

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -5,7 +5,7 @@ module Api
 
       # curl http://localhost:3015/api/products -H "Content-type: application/json" -H 'Authorization: Token token="b76cdabd077990df5d2f2b0679e316c2"'
       def index
-        respond_with Product.visibles.includes(:master, :variants, :main_picture)
+        @products = Product.visibles.includes(:master, :variants, :main_picture)
       end
 
       # curl http://localhost:3015/api/products/:id -H "Content-type: application/json" -H 'Authorization: Token token="b76cdabd077990df5d2f2b0679e316c2"'
@@ -13,7 +13,6 @@ module Api
       # on production, response is {"status":"404","error":"Not Found"}
       def show
         @product = Product.friendly.includes(:master, :variants, :main_picture).find(params[:id])
-        respond_with :api, @product
       end
 
     end
---
+++ app/views/api/v1/products/show.json.jbuilder
@@ -0,0 +1,18 @@
+  json.extract! @product, :id, :name, :current_price
+  json.url product_url(@product, format: :json)
+  json.sku @product.master.sku.to_json
+  json.in_stock @product.master.in_stock.to_json
+
+  json.variants @product.variants do |variant|
+    json.name variant.options['option1']
+    json.sku variant.sku
+    json.in_stock variant.in_stock
+    end
+  json.pictures @product.pictures do |picture|
+    json.name picture.upload_file_name
+    scheme = request.protocol
+    host = request.host_with_port
+    path = picture.upload.url(:thumb)
+    picture_url = URI::Generic::new scheme, nil, host, nil, nil, path, nil, nil, nil
+    json.url picture_url.to_s
+    end
