ed72c0e08612227a8af16d5f49ca10382e574316
Olivier <olivier.simart@gmail.com>
2014-04-26 00:16:43 -0400

    Indentation

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,11 +3,11 @@ class Admin::BaseController < ApplicationController
   before_filter :verify_admin
   before_filter { @checkout_script = true }
 
-private
-    
-  def verify_admin
-  	I18n.locale = 'en' # for now admin is english only
-    redirect_to login_url unless current_user && current_user.admin?
-  end
+  private
+
+    def verify_admin
+  	  I18n.locale = 'en' # for now admin is english only
+      redirect_to login_url unless current_user && current_user.admin?
+    end
 
 end
