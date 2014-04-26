c1aa01d874e6647304973205d04fb3dd396b4cce
Olivier <olivier.simart@gmail.com>
2014-04-26 10:31:18 -0400

    Trailing whitespaces

--- app/models/article.rb
+++ app/models/article.rb
@@ -12,24 +12,24 @@ class Article < ActiveRecord::Base
   attr_reader :tag_tokens
   store :meta_tag, accessors: [:seo_title, :seo_description]
 
-  
+
   # ASSOCIATIONS
   # ------------------------------------------------------------------------------------------------------
   belongs_to :author, class_name: "User", foreign_key: "author_id"
   has_and_belongs_to_many :tags
 
-  
+
   # SCOPES
   # ------------------------------------------------------------------------------------------------------
   scope :visibles, -> { where(visible: true) }
   scope :private,  -> { where(visible: false) }
-  
+
 
   # VALIDATIONS
   # ------------------------------------------------------------------------------------------------------
   validates_presence_of :title, :slug, :author_id, :content
   validates_uniqueness_of :title, :slug
-  
+
 
   # INSTANCE METHODS
   # ------------------------------------------------------------------------------------------------------
