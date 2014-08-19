545ee4f9facd5837a2ee90437694d627348857c8
Olivier <olivier.simart@gmail.com>
2014-08-18 23:35:36 -0400

    Misc.

--- app/controllers/admin/audit_trails_controller.rb
+++ app/controllers/admin/audit_trails_controller.rb
@@ -6,14 +6,14 @@ class Admin::AuditTrailsController < Admin::BaseController
     @audit_trails = AuditTrail.includes(:user).order(sort_column + " " + sort_direction).page(params[:page]).per(30)
   end
 
-private
+  private
 
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
-  end
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
+    end
 
-  def sort_column
-    AuditTrail.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
-  end
+    def sort_column
+      AuditTrail.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
+    end
 
 end
--- app/models/audit_trail.rb
+++ app/models/audit_trail.rb
@@ -1,14 +1,14 @@
 class AuditTrail < ActiveRecord::Base
-  
+
   # ASSOCIATIONS
   # ------------------------------------------------------------------------------------------------------
   belongs_to :auditable, polymorphic: true
   belongs_to :user
-  
+
 
   # SCOPES
   # ------------------------------------------------------------------------------------------------------
   scope :successes, -> { where(success: true) }
-  scope :errors, -> { where(success: false) }
+  scope :errors,    -> { where(success: false) }
 
 end
